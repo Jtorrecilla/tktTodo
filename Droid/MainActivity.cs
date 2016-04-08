4b74412764b587b66d7c7522db0ce9fcd88006f1
Adria Lopez Ortega <adria.lopez.ortega@gmail.com>
2016-04-09 01:21:57 +0200

    Working through Azure

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,7 +7,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-
+using Android.Util;
+using Gcm.Client;
+    
 namespace tktTodoApp.Droid
 {
 	[Activity (Label = "tktTodoApp.Droid", 
@@ -17,14 +19,31 @@ namespace tktTodoApp.Droid
 		Theme = "@android:style/Theme.Holo.Light")]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+        public static MainActivity instance;
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
+        protected override void OnCreate (Bundle bundle)
+		{
+            instance = this;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init (this, bundle);
 			Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
 			LoadApplication (new App ());
-		}
-	}
+
+            RegisterWithGCM();
+        }
+
+        private void RegisterWithGCM()
+        {
+            // Check to ensure everything's set up right
+            GcmClient.CheckDevice(this);
+            GcmClient.CheckManifest(this);
+
+            // Register for push notifications
+            Log.Info("MainActivity", "Registering...");
+            GcmClient.Register(this, Constants.SenderID);
+        }
+    }
 }
 
--- tktTodoApp/Constants.cs
+++ tktTodoApp/Constants.cs
@@ -6,6 +6,10 @@ namespace tktTodoApp
 	{
 		// Replace strings with your mobile services and gateway URLs.
 		public static string ApplicationURL = @"https://tkttodoapp.azurewebsites.net";
-	}
+
+        public const string SenderID = "56068374112"; // Google API Project Number
+        public const string ListenConnectionString = "Endpoint=sb://tokiotaxamarinhub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=O56CUXViIAAUa3h5AxPML8MFBAcA2/aBdtTnQrXYUV8=";
+        public const string NotificationHubName = "tnshub";
+    }
 }
 
